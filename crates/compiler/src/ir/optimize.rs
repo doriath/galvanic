4b4eaacd750c6deff45650790fe643dd945cd0ff
Tomasz Zurkowski <tomasz.zurkowski@gmail.com>
2025-09-01 12:41:10 +0200

    Refactor inline optimization.

--- crates/compiler/src/ir/optimize.rs
+++ crates/compiler/src/ir/optimize.rs
@@ -95,15 +95,46 @@ fn remove_unused_variables(program: &mut Program) -> bool {
     removed_any
 }
 
-#[derive(Default)]
-struct InlineState {
+struct InlineState<'a> {
+    program: &'a mut Program,
     inlined: HashSet<VarId>,
 }
 
-impl InlineState {
-    fn find_var(&self, var_id: VarId, program: &Program) -> (BlockId, usize) {
+impl<'a> InlineState<'a> {
+    pub fn inline_variable(&mut self, id: VarId) {
+        if self.inlined.contains(&id) {
+            return;
+        }
+        self.inlined.insert(id);
+        match self.get_value(id) {
+            VarValue::Single(simple) => {
+                let val = self.inline_simple(&simple);
+                self.set_var(id, val.into());
+            }
+            VarValue::Phi(vars) => {
+                let new_vars = vars
+                    .iter()
+                    .map(|v| self.inline_simple(&(*v).into()))
+                    .collect::<HashSet<_>>();
+                if new_vars.len() == 1 {
+                    self.set_var(id, new_vars.into_iter().next().unwrap().into())
+                }
+            }
+            VarValue::BinaryOp { lhs, op, rhs } => {
+                let lhs = self.inline_simple(&lhs);
+                let rhs = self.inline_simple(&rhs);
+                self.set_var(id, VarValue::BinaryOp { lhs, op, rhs });
+            }
+            VarValue::Call { name, args } => {
+                let args: Vec<VarOrConst> = args.iter().map(|a| self.inline_simple(a)).collect();
+                self.set_var(id, VarValue::Call { name, args });
+            }
+        }
+    }
+
+    fn find_var(&self, var_id: VarId) -> (BlockId, usize) {
         // TODO: optimize this, we should record the location of everything
-        for (block_id, block) in program.blocks.iter().enumerate() {
+        for (block_id, block) in self.program.blocks.iter().enumerate() {
             for (idx, ins) in block.instructions.iter().enumerate() {
                 if let Instruction::Assignment { id, value } = ins {
                     if var_id == *id {
@@ -115,84 +146,38 @@ impl InlineState {
         unreachable!()
     }
 
-    fn inline_variable(self: &mut InlineState, id: VarId, program: &mut Program) {
-        if state.inlined.contains(&id) {
-            return;
+    fn get_value(&self, id: VarId) -> VarValue {
+        let (block_id, idx) = self.find_var(id);
+        if let Instruction::Assignment { id: _, value } =
+            &self.program.blocks[block_id.0].instructions[idx]
+        {
+            return value.clone();
         }
-        state.inlined.insert(id);
-        let get_value = |id: VarId, state: &InlineState, program: &Program| -> VarValue {
-            let (block_id, idx) = state.find_var(id, program);
-            if let Instruction::Assignment { id: _, value } =
-                &program.blocks[block_id.0].instructions[idx]
-            {
-                return value.clone();
-            }
-            unreachable!()
-        };
-        let inline_simple =
-            |v: &VarOrConst, state: &mut InlineState, program: &mut Program| -> VarOrConst {
-                match v {
-                    VarOrConst::Var(id) => {
-                        inline_variable(*id, state, program);
-                        let next_value = get_value(*id, state, program);
-                        if let VarValue::Single(s) = next_value {
-                            return s.clone();
-                        }
-                    }
-                    _ => (),
-                }
-                v.clone()
-            };
-        let (block_id, idx) = state.find_var(id, program);
-        let value = get_value(id, state, program);
-        match value {
-            VarValue::Single(simple) => {
-                let val = inline_simple(&simple, state, program);
-                program.blocks[block_id.0].instructions[idx] = Instruction::Assignment {
-                    id,
-                    value: VarValue::Single(val),
-                };
-            }
-            VarValue::Phi(vars) => {
-                let new_vars = vars
-                    .iter()
-                    .map(|v| inline_simple(&VarOrConst::Var(*v), state, program))
-                    .collect::<HashSet<_>>();
-                if new_vars.len() == 1 {
-                    program.blocks[block_id.0].instructions[idx] = Instruction::Assignment {
-                        id,
-                        value: VarValue::Single(new_vars.into_iter().next().unwrap()),
-                    };
+        unreachable!()
+    }
+
+    fn inline_simple(&mut self, v: &VarOrConst) -> VarOrConst {
+        match v {
+            VarOrConst::Var(id) => {
+                self.inline_variable(*id);
+                let next_value = self.get_value(*id);
+                if let VarValue::Single(s) = next_value {
+                    return s.clone();
                 }
             }
-            VarValue::BinaryOp { lhs, op, rhs } => {
-                let lhs = inline_simple(&lhs, state, program);
-                let rhs = inline_simple(&rhs, state, program);
-                program.blocks[block_id.0].instructions[idx] = Instruction::Assignment {
-                    id,
-                    value: VarValue::BinaryOp { lhs, op, rhs },
-                };
-            }
-            VarValue::Call { name, args } => {
-                let new_args: Vec<VarOrConst> = args
-                    .iter()
-                    .map(|a| inline_simple(a, state, program))
-                    .collect();
-                program.blocks[block_id.0].instructions[idx] = Instruction::Assignment {
-                    id,
-                    value: VarValue::Call {
-                        name,
-                        args: new_args,
-                    },
-                };
-            }
+            _ => (),
         }
+        v.clone()
+    }
+
+    fn set_var(&mut self, id: VarId, value: VarValue) {
+        let (block_id, idx) = self.find_var(id);
+        self.program.blocks[block_id.0].instructions[idx] = Instruction::Assignment { id, value };
     }
 }
 
 // Inlines the variables where possible
 fn inline(program: &mut Program) {
-    let mut state = InlineState::default();
     let mut vars = HashSet::<VarId>::default();
     for b in &program.blocks {
         for ins in &b.instructions {
@@ -201,8 +186,12 @@ fn inline(program: &mut Program) {
             }
         }
     }
+    let mut state = InlineState {
+        program,
+        inlined: HashSet::default(),
+    };
     for id in vars {
-        inline_variable(id, &mut state, program);
+        state.inline_variable(id);
     }
     return;
 }
